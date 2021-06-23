c4c8d8f8a1c8c06f8c289d8e0f7f45fd21b26d2c
High Speed <dszulc.dev@gmail.com>
2021-06-23 15:55:14 +0200

    commit

---
+++ modelTraining.py
@@ -0,0 +1,15 @@
+import os
+import pathlib
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import models
+
+data_dir = pathlib.Path('dataset/en')
+
+def main():
+    #
+    return
+
+
+if __name__ == '__main__':
+    main()
